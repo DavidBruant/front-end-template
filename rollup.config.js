8b6fe218d45f7f4d5bcef3d11cf024ebafc92834
David Bruant <davidbruant@protonmail.com>
2023-02-27 19:08:46 +0100

    remove unused live reload

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-//import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 import sveltePreprocess from 'svelte-preprocess'
@@ -38,10 +37,6 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
-
-		// Watch the `_site` directory and refresh the
-		// browser on changes when not in production
-		//!production && livereload('_site'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
